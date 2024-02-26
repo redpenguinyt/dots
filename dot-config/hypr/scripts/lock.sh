0b564c89feebe66406e1afde3ac8dc319d177c4f
redpenguinyt <redpenguin777@yahoo.com>
2024-02-26 21:24:44 +0000

    Padded 24 hour clock

--- dot-config/hypr/scripts/lock.sh
+++ dot-config/hypr/scripts/lock.sh
@@ -5,7 +5,7 @@ swaylock \
     --grace 5 \
     --clock --indicator \
     --font "CaskaydiaMono Nerd Font" --text-color 93b5af \
-    --datestr "%a %e.%m.%Y" --timestr "%k:%M" \
+    --datestr "%a %e.%m.%Y" --timestr "%H:%M" \
     --inside-color 1a1d2c -n \
     --indicator-radius 300 \
     --ring-color 8b88db --key-hl-color 91bfff \
