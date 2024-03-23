cf1fd15c712ec7efd25f511a82548b1f23fa49e8
redpenguinyt <redpenguin777@yahoo.com>
2024-03-23 23:57:47 +0000

    replcae dots with slashes for date

--- dot-config/hypr/scripts/lock.sh
+++ dot-config/hypr/scripts/lock.sh
@@ -4,7 +4,7 @@ swaylock \
     --color 151426 \
     --clock --indicator \
     --font "CaskaydiaMono Nerd Font" --text-color 93b5af \
-    --datestr "%a %e.%m.%Y" --timestr "%H:%M" \
+    --datestr "%a %e/%m/%Y" --timestr "%H:%M" \
     --inside-color 1a1d2c -n \
     --indicator-radius 300 \
     --ring-color 8b88db --key-hl-color 91bfff \
