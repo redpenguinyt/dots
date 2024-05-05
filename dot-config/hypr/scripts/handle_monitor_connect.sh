7550cdae990e8265e99097bd54a72c1f227375f3
Red Penguin <redpenguin777@yahoo.com>
2024-05-05 14:56:34 +0100

    move workspaces to connected monitor

---
+++ dot-config/hypr/scripts/handle_monitor_connect.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+handle() {
+  case $1 in monitoradded*)
+    hyprctl dispatch moveworkspacetomonitor "1 1"
+    hyprctl dispatch moveworkspacetomonitor "2 1"
+    hyprctl dispatch moveworkspacetomonitor "3 1"
+  esac
+}
+
+socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" | while read -r line; do handle "$line"; done
