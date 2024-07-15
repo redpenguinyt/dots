7b29cd1a3dccf245ef0f6c76fb9b1b34179961d7
Red Penguin <redpenguin777@yahoo.com>
2024-07-15 23:54:20 +0100

    fixed audio script

--- dot-config/hypr/scripts/audio.sh
+++ dot-config/hypr/scripts/audio.sh
@@ -4,7 +4,7 @@ export CARD=$(pactl list cards short | head -n 1 | awk '{print $2}')
 
 export audioSetting=$(pactl info | grep "^Default Sink" | cut -d'.' -f4)
 export monitorsLen=$(hyprctl monitors -j | jq 'length')
-if [ audioSetting == "analog-stereo" ] && [ monitorsLen > 1 ]; then
+if [ "$audioSetting" = "analog-stereo" ] && [ monitorsLen > 1 ]; then
     pactl set-card-profile $CARD output:hdmi-stereo+input:analog-stereo
 else
     pactl set-card-profile $CARD output:analog-stereo+input:analog-stereo
