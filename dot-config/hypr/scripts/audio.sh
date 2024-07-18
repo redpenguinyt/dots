f6933fdac7690d1756685a798c0db88008292757
Red Penguin <redpenguin777@yahoo.com>
2024-07-18 19:48:27 +0100

    fix audio script issues - make sure to search for pci card in case a microphone is plugged in - compare monitorsLen in arithmetic context

--- dot-config/hypr/scripts/audio.sh
+++ dot-config/hypr/scripts/audio.sh
@@ -1,10 +1,10 @@
 # Toggle between HDMI and built-in output
 
-export CARD=$(pactl list cards short | head -n 1 | awk '{print $2}')
+export CARD=$(pactl list cards short | grep pci | head -n 1 | awk '{print $2}')
 
 export audioSetting=$(pactl info | grep "^Default Sink" | cut -d'.' -f4)
 export monitorsLen=$(hyprctl monitors -j | jq 'length')
-if [ "$audioSetting" = "analog-stereo" ] && [ monitorsLen > 1 ]; then
+if [ "$audioSetting" = "analog-stereo" ] && (( monitorsLen > 1 )); then
     pactl set-card-profile $CARD output:hdmi-stereo+input:analog-stereo
 else
     pactl set-card-profile $CARD output:analog-stereo+input:analog-stereo
