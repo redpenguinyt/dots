3fa1e38c6e5bdc16e3f5db32508d893908e2e8e9
Red Penguin <redpenguin777@yahoo.com>
2024-10-15 20:45:38 +0100

    viv.sh - set 3 second seek interval

--- modules/desktop/hypr/scripts/viv.sh
+++ modules/desktop/hypr/scripts/viv.sh
@@ -1,1 +1,7 @@
-ffplay -autoexit -volume 30 -vf scale=-1:800 -i "$*"
+# Nice MacOS-like video preview to be used in file manager
+ffplay \
+    -autoexit \
+    -volume 30 \
+    -vf scale=-1:800 \
+    -seek_interval 3 \
+    -i "$*"
