1cbe742b67d5a777f842a5f9dbab4106f7460e59
Red Penguin <redpenguin777@yahoo.com>
2024-12-10 20:25:00 +0000

    hypr script

--- modules/desktop/hypr/scripts/audio.sh
+++ modules/desktop/hypr/scripts/audio.sh
@@ -4,7 +4,7 @@ export SINKS=$(wpctl status -k | rg Sinks: -A 2 -m 1 | tail -n +2 | cut -c 7-)
 echo "Available Sinks"
 echo "$SINKS"
 
-export NEW_SINK_ID=$(echo "$SINKS" | rg -v "\*" | cut -c 5-6)
+export NEW_SINK_ID=$(echo "$SINKS" | rg -v "\*" | cut -c 4-6)
 echo "Switching to sink $NEW_SINK_ID"
 
 wpctl set-default "$NEW_SINK_ID"
